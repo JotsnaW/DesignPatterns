504d34261a98e77641389c78c67a2919ae4352ae
Jotsna Waghmare <jotsna.d.w@gmail.com>
2024-02-07 19:46:54 +0530

    new files for Singleton program

---
+++ SingletonDesignPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SingletonDesignPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // call to get the instance of Singleton class
+            Singleton SingletonInstance1 = Singleton.GetInstance();
+
+            SingletonInstance1.setValue("Jotsna Waghmare");
+
+            Singleton SingletonInstance2 = Singleton.GetInstance();
+
+            Console.WriteLine(SingletonInstance2.getValue());
+
+        }
+    }
+}
