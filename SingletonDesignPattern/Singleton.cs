14049c9593bad149c33f77a144d02f4095d8153c
Jotsna Waghmare <jotsna.d.w@gmail.com>
2024-02-07 19:39:29 +0530

    Singleton Design Pattern Program
    Simple program on Singleton Design Pattern.

---
+++ SingletonDesignPattern/Singleton.cs
@@ -0,0 +1,31 @@
+﻿namespace SingletonDesignPattern
+{
+    internal class Singleton
+    {
+        private string name;
+
+        private static Singleton instance = new Singleton();
+
+        // private çonstructor
+        private Singleton()
+        {
+
+        }
+
+        // static method
+        public static Singleton GetInstance()
+        {
+            return instance;
+        }
+
+        public string getValue()
+        {
+            return name;
+        }
+
+        public void setValue(string text)
+        {
+            name = text;
+        }
+    }
+}
