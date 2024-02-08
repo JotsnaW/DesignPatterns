11692fda00438dd2388329da71740008fdecc817
Jotsna Waghmare <jotsna.d.w@gmail.com>
2024-02-08 17:56:45 +0530

    Code for Multiple Loggers using Singleton Design pattern
    Problem Statement - Create multiple loggers writing to the same log file in parallel. Use the singleton pattern to ensure only a single logger can be instantiated for a given file.

---
+++ MultiLoggerSingleton/Logger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace MultiLoggerSingleton
+{
+    public class Logger
+    {
+        public static int count =0;
+        private readonly string m_logFile;
+        private readonly static Logger instance = new Logger();
+
+        // private constructor
+        private Logger() 
+        { 
+
+            m_logFile = "D:\\GitHub\\DesignPatterns\\MultiLoggerSingleton\\logFile.txt";
+            count++;
+        }
+
+        // static method to get instance
+        public static Logger GetInstance()
+        {
+            return instance;
+        } 
+
+        // method to log message to file
+        public void LogToFile(string logMessage)
+        {
+            Console.WriteLine($"Logging {logMessage} to file: {m_logFile}");
+            File.AppendAllText(m_logFile, logMessage);
+        }
+    }
+}
---
+++ MultiLoggerSingleton/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MultiLoggerSingleton
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            Logger logger1 = Logger.GetInstance();
+            logger1.LogToFile("log1- singleton pattern" + Environment.NewLine);
+
+            Logger logger2 = Logger.GetInstance();
+            logger2.LogToFile("log2- singleton pattern" + Environment.NewLine);
+
+            Logger logger3 = Logger.GetInstance();
+            logger3.LogToFile("log3- singleton pattern" + Environment.NewLine);
+
+            Console.WriteLine($"Constructor is called for {Logger.count} times.");
+        }
+    }
+}
