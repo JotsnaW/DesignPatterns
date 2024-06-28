2fd814ba2184dd04339c1508d048fa993e656d94
Jotsna.Waghmare <Jotsna.Waghmare@bentley.com>
2024-06-28 16:20:40 +0530

    Example of Undo/redo functionalities in TextEditor Using Memento
    Example of Undo/redo functionalities in TextEditor Using Memento

---
+++ Memento/MementoDesignPattern/TextEditor_UndoRedo/TextEditor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TextEditor
+{
+    //Originator class
+    public class TextEditor
+    {
+        public string Text { get; set; }
+
+        public IMemento CreateMemento()
+        {
+            return new Memento(Text);
+        }
+
+        public void RestoreMemento(IMemento memento)
+        {
+            if (memento != null)
+                Text = memento.Text;
+            else
+                Console.WriteLine("memento is empty.");
+        }
+
+        
+    }
+}
